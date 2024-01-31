b20c37650328f51c22e3d226bd07faf48211b5ff
Willame Mouzinho <willame.dev@gmail.com>
2024-01-31 12:14:28 -0300

    feat: add footer component

---
+++ web/src/components/footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer className="w-full bg-card">
+      <div className="max-w-[1224px] w-full mx-auto px-5 py-6">
+        <p className="text-xs text-zinc-400">
+          © 2023 Copyright <span className="font-bold">FSW Barber</span>
+        </p>
+      </div>
+    </footer>
+  )
+}
