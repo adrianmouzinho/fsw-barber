ff807d0dbfef15856316ea8e90429247af74b867
Willame Mouzinho <willame.dev@gmail.com>
2024-01-31 11:07:42 -0300

    feat: add barbershop card component

--- web/src/app/(home)/components/barbershop-card.tsx
+++ web/src/app/(home)/components/barbershop-card.tsx
@@ -1,20 +1,31 @@
+import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
+import { StarIcon } from 'lucide-react'
 import Image from 'next/image'
 
 export function BarbershopCard() {
   return (
     <Card className="rounded-2xl">
-      <CardContent className="p-1">
-        <div>
+      <CardContent className="p-0">
+        <div className="relative max-w-[168px] w-full p-1 flex flex-col">
           <Image
             src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
             alt="Vintage Barber"
             width={160}
             height={160}
-            className="rounded-xl"
+            className="min-w-40 min-h-40 mb-2 rounded-xl object-cover"
           />
-          <div className="px-2">
-            <h3>Vintage Barber</h3>
+          <span className="absolute top-2 left-2 w-fit py-1.5 px-2.5 flex items-center gap-0.5 text-xs font-bold bg-[#221C3D]/70 backdrop-blur-[3px] rounded-full">
+            <StarIcon className="text-primary size-3 fill-primary" /> 5,0
+          </span>
+          <div className="px-2 flex flex-col gap-3">
+            <div className="flex flex-col gap-1">
+              <h3 className="font-bold truncate">Vintage Barber</h3>
+              <p className="text-xs text-zinc-400">
+                Avenida São Sebastião, 357, São Paulo
+              </p>
+            </div>
+            <Button variant="secondary">Reservar</Button>
           </div>
         </div>
       </CardContent>
